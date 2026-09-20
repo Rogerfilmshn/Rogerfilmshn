42790d1ca631fb6b3d2c28c877cf4485130c82aa
Roger Films hn <107081076+Rogerfilmshn@users.noreply.github.com>
2026-09-20 12:13:56 -0600

    Update social config API URL

--- assets/social-config.js
+++ assets/social-config.js
@@ -1,2 +1,5 @@
 // Public endpoint only. Never put credentials here. Empty keeps counters hidden until activation.
 window.ROGERFILMS_SOCIAL = Object.freeze({ apiUrl: '' });
+window.ROGERFILMS_SOCIAL = Object.freeze({
+  apiUrl: 'https://rogerfilms-social.rogerfilmshn.workers.dev'
+});
